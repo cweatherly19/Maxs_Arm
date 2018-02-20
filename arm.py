8a7a23a2743590e562fb2d29e12b4b4516925a78
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-02-20 10:17:40 -0600

    Update arm.py

--- arm.py
+++ arm.py
@@ -4,6 +4,6 @@ motor = 1
 print "press y to go, then n to stop"
 start = raw_input("")
 while start == "y":
-  RPL.servoWrite(motor, 1000)
+  RPL.servoWrite(motor, 2500)
   if "n":
       RPL.servoWrite(motor, 0)
