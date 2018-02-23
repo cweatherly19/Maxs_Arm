d124b0e6668772be3b76e4fefcc29a33be4d1ce0
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-02-23 10:08:54 -0600

    Update arm.py

--- arm.py
+++ arm.py
@@ -4,24 +4,24 @@ motor = 1
 print "press y to go down, press a to go up, or n to stop"
 start = raw_input("")
 while true:
-  while start == "y":
-   RPL.servoWrite(motor, 2500)
-   print "press n to stop"
-   start1 = raw_input("")
-   if start1 == "n":
-     break
-   if start1 == "a":
-     break
-  while start == "a":
-   RPL.servoWrite(motor, 1000)
-   print "press n to stop"
-   start2 = raw_input("")
-   if start2 == "n":
-     break
-   if start1 == "y":
-     break
-  while start == "n":
-   RPL.servoWrite(motor, 0)
-   break
-if rawinput("") == "x"
-  break
+    while start == "y":
+        RPL.servoWrite(motor, 2500)
+        print "press n to stop"
+        start1 = raw_input("")
+        if start1 == "n":
+            break
+        if start1 == "a":
+            break
+    while start == "a":
+        RPL.servoWrite(motor, 1000)
+        print "press n to stop"
+        start2 = raw_input("")
+        if start2 == "n":
+            break
+        if start1 == "y":
+            break
+    while start == "n":
+        RPL.servoWrite(motor, 0)
+        break
+if rawinput("") == "x":
+    break
