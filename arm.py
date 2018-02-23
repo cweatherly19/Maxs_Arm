a8294a9952d37792c45cb0f4b1e7b9207229343d
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-02-23 09:49:14 -0600

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,12 +1,19 @@
 import setup
 import RoboPiLib as RPL
 motor = 1
-print "press y to go, then n to stop"
+print "press y to go down, press a to go up, or n to stop"
 start = raw_input("")
 while start == "y":
   RPL.servoWrite(motor, 2500)
-  print "press y to go, then n to stop"
+  print "press n to stop"
   start1 = raw_input("")
   if start1 == "n":
+    break
+    RPL.servoWrite(motor, 0)
+while start == "a":
+  RPL.servoWrite(motor, 1000)
+  print "press n to stop"
+  start2 = raw_input("")
+  if start2 == "n":
     break
     RPL.servoWrite(motor, 0)
