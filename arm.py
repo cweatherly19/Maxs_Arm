3346b000f2ccf85954786eb9480965372ab9d041
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-03-07 10:43:44 -0600

    Update arm.py

--- arm.py
+++ arm.py
@@ -7,16 +7,25 @@ motor = 1
 print "press y to go down, press a to go up, n to stop"
 run = raw_input("> ")
 def arm_up( up ):
-    if run == "a" or "n":
-        arm_up(RPL.servoWrite(motor, 0))
+    future = time.time() + 2
+    while time.time() > future:
+        arm_up(RPL.servoWrite(motor, 2000))
+        print "y"
+        run = raw_input("> ")
     return up
 def arm_down( down ):
-    if run == "y" or "n":
-        arm_up(RPL.servoWrite(motor, 0))
+    future = time.time() + 2
+    while time.time() > future:
+        arm_up(RPL.servoWrite(motor, 1000))
+        print "a"
+        run = raw_input("> ")
     return down
 def arm_stop( still ):
-    if run == "y" or "a":
-        arm_up(RPL.servoWrite(motor, 0))
+    future = time.time() + 2
+    while time.time() > future:
+        arm_stop(RPL.servoWrite(motor, 0))
+        run = raw_input("> ")
+        print "n"
     return still
 
 # arm_up()
@@ -25,20 +34,17 @@ def arm_stop( still ):
 
 while True:
     if run == "y":
-        future = time.time() + 2
-        while time.time() > future:
-            arm_up(RPL.servoWrite(motor, 2000))
-            print "y"
-            run = raw_input("> ")
+        if run == "a" or "n":
+            future = time.time() + 2
+            while time.time() > future:
+                arm_up(RPL.servoWrite(motor, 0))
     if run == "a":
-        future = time.time() + 2
-        while time.time() > future:
-            arm_down(RPL.servoWrite(motor, 1000))
-            run = raw_input("> ")
-            print "a"
+        if run == "y" or "n":
+            future = time.time() + 2
+            while time.time() > future:
+                arm_down(RPL.servoWrite(motor, 0))
     if run == "n":
-        future = time.time() + 2
-        while time.time() > future:
-            arm_stop(RPL.servoWrite(motor, 0))
-            run = raw_input("> ")
-            print "n"
+        if run == "a" or "y":
+            future = time.time() + 2
+            while time.time() > future:
+                arm_stop(RPL.servoWrite(motor, 0))
