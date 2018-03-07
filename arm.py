2cac3d220043db25bc678e634d19babc195b3065
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-03-07 10:09:07 -0600

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,29 +1,41 @@
 import setup
 import RoboPiLib as RPL
+import time
+now = time.time()
+future = now
 motor = 1
 print "press y to go down, press a to go up, n to stop"
-true = raw_input("> ")
+run = raw_input("> ")
 def arm_up( up ):
+    if run == a or n
+    arm_up(RPL.servoWrite(motor, 0))
     return;
 def arm_down( down ):
     return;
 def arm_stop( still ):
     return;
 
-arm_up(RPL.servoWrite(motor, 2000))
-arm_down(RPL.servoWrite(motor, 1000))
-arm_stop(RPL.servoWrite(motor, 0))
+# arm_up()
+# arm_down()
+# arm_stop(RPL.servoWrite(motor, 0))
 
 while True:
-    if true == "y":
-        arm_up()
-        print "y"
-    if true == "a":
-        arm_down()
-        print "a"
-    if true == "n":
-        arm_stop()
-        print "n"
-    if true == "x":
-        arm_stop()
-        print "x"
+    if run == "y":
+        future = time.time() + 2
+        while time.time() > future:
+            arm_up(RPL.servoWrite(motor, 2000))
+            print "y"
+            run = raw_input("> ")
+            return run
+    if run == "a":
+        future = time.time() + 2
+        while time.time() > future:
+            arm_down(RPL.servoWrite(motor, 1000))
+            run = raw_input("> ")
+            print "a"
+    if run == "n":
+        future = time.time() + 2
+        while time.time() > future:
+            arm_stop(RPL.servoWrite(motor, 0))
+            run = raw_input("> ")
+            print "n"
