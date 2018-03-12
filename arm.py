bb0dd90c38df3d225d275108e80dc473948e8f0d
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-03-12 09:44:03 -0500

    Update arm.py

--- arm.py
+++ arm.py
@@ -6,12 +6,16 @@ future = now
 motor = 1
 print "press y to go down, press a to go up, n to stop"
 run = raw_input("> ")
+#nothing to run code yet
 def arm_up( up ):
     future = time.time() + 2
     while time.time() > future:
         arm_up(RPL.servoWrite(motor, 2000))
         print "y"
         run = raw_input("> ")
+            if run == "a" or "n":
+                break
+#the above should work
     return up
 def arm_down( down ):
     future = time.time() + 2
@@ -34,17 +38,23 @@ def arm_stop( still ):
 
 while True:
     if run == "y":
+        arm_up( up )
         if run == "a" or "n":
             future = time.time() + 2
             while time.time() > future:
                 arm_up(RPL.servoWrite(motor, 0))
+                break
     if run == "a":
+        arm_down( down )
         if run == "y" or "n":
             future = time.time() + 2
             while time.time() > future:
                 arm_down(RPL.servoWrite(motor, 0))
+                break
     if run == "n":
+        arm_stop( still )
         if run == "a" or "y":
             future = time.time() + 2
             while time.time() > future:
                 arm_stop(RPL.servoWrite(motor, 0))
+                break
