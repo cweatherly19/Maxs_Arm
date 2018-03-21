65eb840090c39e59a29e6dd8ddd322e33ad78dae
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-03-21 10:43:25 -0500

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,78 +1,84 @@
 import time
 import setup
 import RoboPiLib as RPL
+#up and down
 motor1 = 1
+#left and right
 motor2 = 0
-print "press s to go down, press w to go up,"
-print "a to turn left, d to turn right,"
-print "and any other key to end code."
+#tells the commands
+print "press y to go down, press a to go up, and any other key to end code"
+#allows for inputs
 command = raw_input("> ")
-def speed( turn ):
-    print turn
+#reads the turn in the loop below
+def run( turn ):
     return turn
-def run( go ):
-    print go
+#reads the up and down in the loops
+def speed( go ):
     return go
-x = 0
+#allows code to break and stop completely
 z = 0
-while x is 0:
-    y = 1
-    while y is 1 and command is "w":
-        now = time.time()
-        future = 1 + now
-        while y is 1 and now < future:
-            speed(RPL.servoWrite(motor1,1000))
-            while y is 1 and time.time() >= future:
-                speed(RPL.servoWrite(motor1,0))
-                print "insert new input"
-                y = 2
-    if command is "w":
-        command = raw_input("> ")
-        continue
-    while y is 1 and command is "s":
-        now = time.time()
-        future = 1 + now
-        while y is 1 and now < future:
-            speed(RPL.servoWrite(motor1,2000))
-            while y is 1 and time.time() >= future:
-                speed(RPL.servoWrite(motor1,0))
-                print "insert new input"
-                y = 2
-    if command is "s":
-        command = raw_input("> ")
-        continue
-    while command is not "w" or "s" or "a" or "d":
-        print "program stopping"
-        speed(RPL.servoWrite(motor1,0))
-        x = 1
-        break
+#loop to run robot
 while z is 0:
+    #allows individual loops to break
     v = 1
+    #turn right
     while v is 1 and command is "d":
-        now = time.time()
-        future = 1 + now
-        while v is 1 and now < future:
-            run(RPL.servoWrite(motor2,1000))
-            while v is 1 and time.time() >= future:
-                run(RPL.servoWrite(motor2,0))
-                print "insert new input"
-                v = 2
+        run(RPL.servoWrite(motor2,1000))
+        print "insert new input"
+        #breaks loop
+        v = 2
+    #return to top of loop if "d"
     if command is "d":
         command = raw_input("> ")
         continue
+    #turn left
     while v is 1 and command is "a":
-        now = time.time()
-        future = 1 + now
-        while v is 1 and now < future:
-            run(RPL.servoWrite(motor2,2800))
-            while v is 1 and time.time() >= future:
-                run(RPL.servoWrite(motor2,0))
-                print "insert new input"
-                v = 2
+        run(RPL.servoWrite(motor2,2800))
+        print "insert new input"
+        #breaks loop
+        v = 2
+    #return to top of loof if "a"
     if command is "a":
         command = raw_input("> ")
         continue
-    while command is not "w" or "s" or "a" or "d":
-        run(RPL.servoWrite(motor1,0))
-        v = 1
+    #go up
+    while v is 1 and command is "w":
+        #sets amount of time for motor to run
+        now = time.time()
+        future = 1 + now
+        #starts motor running
+        while v is 1 and now < future:
+            speed(RPL.servoWrite(motor1,1000))
+            #ends motor running
+            while v is 1 and time.time() >= future:
+                speed(RPL.servoWrite(motor1,0))
+                print "insert new input"
+                #breaks loops
+                v = 2
+    #return to top of loop if "w"
+    if command is "w":
+        command = raw_input("> ")
+        continue
+    #go down
+    while v is 1 and command is "s":
+        #set emount of time for motor to run
+        now = time.time()
+        future = 1 + now
+        #starts motor running
+        while v is 1 and now < future:
+            speed(RPL.servoWrite(motor1,2000))
+            #ends motor running
+            while v is 1 and time.time() >= future:
+                speed(RPL.servoWrite(motor1,0))
+                print "insert new input"
+                v = 2
+    #return to top of loop if "s"
+    if command is "s":
+        command = raw_input("> ")
+        continue
+    #stops code
+    while command is not "a" or "d" or "w" or "s":
+        print "program stopping"
+        speed(RPL.servoWrite(motor1,0))
+        z = 1
         break
