d00f010571eefc55716393cac16b74b1c1c32aac
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-03-21 09:49:24 -0500

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,60 +1,40 @@
+import time
 import setup
 import RoboPiLib as RPL
-import time
-now = time.time()
-future = now
 motor = 1
-print "press y to go down, press a to go up, n to stop"
-run = raw_input("> ")
-#nothing to run code yet
-def arm_up( up ):
-    future = time.time() + 2
-    while time.time() > future:
-        arm_up(RPL.servoWrite(motor, 2000))
-        print "y"
-        run = raw_input("> ")
-        if run == "a" or "n":
-            break
-#the above should work
-    return up
-def arm_down( down ):
-    future = time.time() + 2
-    while time.time() > future:
-        arm_up(RPL.servoWrite(motor, 1000))
-        print "a"
-        run = raw_input("> ")
-    return down
-def arm_stop( still ):
-    future = time.time() + 2
-    while time.time() > future:
-        arm_stop(RPL.servoWrite(motor, 0))
-        run = raw_input("> ")
-        print "n"
-    return still
-
-# arm_up()
-# arm_down()
-# arm_stop(RPL.servoWrite(motor, 0))
-
-while True:
-    if run == "y":
-        arm_up( up )
-        if run == "a" or "n":
-            future = time.time() + 2
-            while time.time() > future:
-                arm_up(RPL.servoWrite(motor, 0))
-                break
-    if run == "a":
-        arm_down( down )
-        if run == "y" or "n":
-            future = time.time() + 2
-            while time.time() > future:
-                arm_down(RPL.servoWrite(motor, 0))
-                break
-    if run == "n":
-        arm_stop( still )
-        if run == "a" or "y":
-            future = time.time() + 2
-            while time.time() > future:
-                arm_stop(RPL.servoWrite(motor, 0))
-                break
+print "press y to go down, press a to go up, and any other key to end code"
+command = raw_input("> ")
+def speed( go ):
+    return go
+x = 0
+while x is 0:
+    y = 1
+    while y is 1 and command is "a":
+        now = time.time()
+        future = 1 + now
+        while y is 1 and now < future:
+            speed(RPL.servoWrite(motor,1000))
+            while y is 1 and time.time() >= future:
+                speed(RPL.servoWrite(motor,0))
+                print "insert new input"
+                y = 2
+    if command is "a":
+        command = raw_input("> ")
+        continue
+    while y is 1 and command is "y":
+        now = time.time()
+        future = 1 + now
+        while y is 1 and now < future:
+            speed(RPL.servoWrite(motor,2000))
+            while y is 1 and time.time() >= future:
+                speed(RPL.servoWrite(motor,0))
+                print "insert new input"
+                y = 2
+    if command is "y":
+        command = raw_input("> ")
+        continue
+    while command is not "y" or "a":
+        print "program stopping"
+        speed(RPL.servoWrite(motor,0))
+        x = 1
+        break
