0677f9cf55897b8035bdbe494fc95db033dbacb4
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-04-18 10:53:44 -0500

    Update arm.py

--- arm.py
+++ arm.py
@@ -8,43 +8,38 @@ motor2 = 0
 print "press w to go down, press s to go up,"
 print "a to go left, d to go right,"
 print "and any other key to end code."
-#reads the turn in the loop below
-def run( turn ):
-    return turn
-#reads the up and down in the loops
-def speed( go ):
-    return go
+
 #allows code to break and stop completely
 z = 0
 #loop to run robot
 while z is 0:
     #allows individual loops to break
     v = 1
-    speed(RPL.servoWrite(motor1, 0))
+    RPL.servoWrite(motor1, 0)
     #turn right
     while v is 1 and raw_input("> ") is "d":
-        run(RPL.servoWrite(motor2, 800))
+        RPL.servoWrite(motor2, 800)
         #breaks loop
         if raw_input("> ") is not "d":
             v = 2
             continue
     #turn left
     while v is 1 and raw_input("> ") is "a":
-        run(RPL.servoWrite(motor2, 3000))
+        RPL.servoWrite(motor2, 3000)
         #breaks loop
         if raw_input("> ") is not "a":
             v = 2
             continue
     #go up
     while v is 1 and raw_input("> ") is "w":
-        speed(RPL.servoWrite(motor1, 1000))
+        RPL.servoWrite(motor1, 1000)
         #breaks loops
         if raw_input("> ") is not "w":
             v = 2
             continue
     #go down
-    while v is 1 and raw_input("> ") is "s":
-        speed(RPL.servoWrite(motor1, 2000))
+    while v is 1 and war_input("> ") is "s":
+        RPL.servoWrite(motor1, 2000)
         if raw_input("> ") is not "s":
             v = 2
             continue
