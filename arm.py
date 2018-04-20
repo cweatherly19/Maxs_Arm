0a32cd9abed2b2c2d70836d0d6f72478cd20fb45
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-04-20 10:00:57 -0500

    Update arm.py

--- arm.py
+++ arm.py
@@ -14,41 +14,28 @@ command = raw_input("> ")
 z = 0
 #loop to run robot
 while z is 0:
-    #allows individual loops to break
-    v = 1
-    RPL.servoWrite(motor1, 0)
     #turn right
-    while v is 1 and command is "d":
+    while command is "d":
         RPL.servoWrite(motor2, 800)
         #breaks loop
-        v = 2
-    if command is "d":
         command = raw_input("> ")
         continue
     #turn left
-    while v is 1 and command is "a":
+    while command is "a":
         RPL.servoWrite(motor2, 3000)
         #breaks loop
-        if command is not "a":
-            v = 2
-    if command is "a":
         command = raw_input("> ")
         continue
     #go up
-    while v is 1 and command is "w":
+    while command is "w":
         RPL.servoWrite(motor1, 1000)
         #breaks loops
-        if command is not "w":
-            v = 2
-    if command is "w":
         command = raw_input("> ")
         continue
     #go down
-    while v is 1 and command is "s":
+    while command is "s":
         RPL.servoWrite(motor1, 2000)
         if command is not "s":
-            v = 2
-    if command is "s":
         command = raw_input("> ")
         continue
     #stops code
