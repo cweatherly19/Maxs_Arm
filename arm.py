a71aae6b295a165329b691f51f1882cb0f4c9f7a
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-05-02 10:59:23 -0500

    Update arm.py

--- arm.py
+++ arm.py
@@ -10,30 +10,40 @@ position2 = 800
 #to define the 'screen' in front of functions
 screen = curses.initscr()
 #to tell the user valid inputs
-screen.addstr('Hit q to quit. Use the W, A, S, and D to move.')
+screen.addstr('Hit q to quit. Use the w, a, s, and d to move. Hit e to find positions of motor 2')
 #to read key inputs
 key = ''
 #to end loop if 'q' is hit
 while key != ord('q'):
     #so the key can be read
     key = screen.getch()
+    screen.clear()
+    screen.addstr('Hit q to quit. Use the w, a, s, and d to move. Hit e to find positions of motor 2')
+    screen.addstr('Detected key: ')
     #to define what keys preform commands
     if key == ord('w'):
+        screen.addstr('w key')
         RPL.servoWrite(motor1, 1000)
     if key == ord('a'):
+        screen.addstr('a key')
         position2 = position2 + 50
         #to allow for a full rotation
         if position2 > 3000:
             position2 = 800
         RPL.servoWrite(motor2, position2)
     if key == ord('s'):
+        screen.addstr('k key')
         RPL.servoWrite(motor1, 2000)
     if key == ord('d'):
+        screen.addstr('d key')
         position2 = position2 - 50
         #to allow for a full rotation
         if position2 < 800:
             position2 = 3000
         RPL.servoWrite(motor2, position2)
     RPL.servoWrite(motor1, 0)
+    if key == ord('e'):
+        screen.addstr('e key')
+        screen.addstr('Value of motor 2: %d' %position2)
 #to reformat the terminal/end the curses program
 curses.endwin()
