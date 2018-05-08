1b3fb8760c9bd76a9237c08ea2862f2446cd8f15
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-05-08 09:59:18 -0500

    Update arm.py

--- arm.py
+++ arm.py
@@ -10,7 +10,7 @@ position2 = 800
 #to define the 'screen' in front of functions
 screen = curses.initscr()
 #to tell the user valid inputs
-screen.addstr('Hit q to quit. Use the w, a, s, and d to move. Value of Motor 2: %d' %position2)
+screen.addstr('Hit q to quit. Use the w, a, s, and d to move. Value of Motor: %d' %position2)
 #to read key inputs
 key = ''
 #to end loop if 'q' is hit
@@ -18,7 +18,7 @@ while key != ord('q'):
     #so the key can be read
     key = screen.getch()
     screen.clear()
-    screen.addstr('Hit q to quit. Use the w, a, s, and d to move. Position of Motor 2: %d' %position2)
+    screen.addstr('Hit q to quit. Use the w, a, s, and d to move. Position of Motor: %d' %position2)
     screen.addstr('Detected key: ')
     #to define what keys preform commands
     if key == ord('w'):
@@ -41,6 +41,8 @@ while key != ord('q'):
         if position2 < 800:
             position2 = 3000
         RPL.servoWrite(motor2, position2)
-    RPL.servoWrite(motor1, 0)
+    key = screen.getch()
+    else:
+        RPL.servoWrite(motor1, 0)
 #to reformat the terminal/end the curses program
 curses.endwin()
